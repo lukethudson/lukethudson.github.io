50bd42671432687509e752a6e7036895702a2c68
anth0nyhle <anthony.le@utah.edu>
2023-01-20 11:20:45 -0700

    Add febio project

--- ConvertLatex2HTML.R
+++ ConvertLatex2HTML.R
@@ -3,5 +3,5 @@ library(pandoc)
 # set working directory 
 setwd(getwd())
 
-# convert cv latex file to html file
-pandoc_convert(file='./GitHub/anth0nyhle.github.io/cv.tex', from='latex', to='html', output='./GitHub/anth0nyhle.github.io/cv.html')
+# convert cv latex file to html
+pandoc_convert(file='../anth0nyhle.github.io/cv.tex', from='latex', to='html', output='../anth0nyhle.github.io/cv.html')
